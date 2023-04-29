b2c73f4872191abf7eeac76413501b016efd368f
kevin-wairi <kevinwairi@gmail.com>
2023-04-29 15:22:33 +0300

    assignment completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const elem = directors.map((director,index)=>{
+    return (
+      < div key={index}>
+        <h4>{director.name}</h4>
+        <ul>
+          {director.movies.map((movie,index) => {
+            return ( <li key={index}>{movie}</li>)
+          })}
+        </ul>
+      </div>
+    );
+  })
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {elem}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const elem =movies.map((movie,index)=>{
+             return (
+               <div key={index}>
+                 <h4>Name:{movie.title}</h4>
+                 <p>Time:{movie.time}</p>
+                 <p>Genres:</p>
+                 <ul>
+                   {movie.genres.map((genre,index) => {
+                     return <li key={index}>{genre}</li>;
+                   })}
+                 </ul>
+               </div>
+             );
+                
+        })
+    
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {elem}
+    </div>
+  );
 }
 
 export default Movies;
